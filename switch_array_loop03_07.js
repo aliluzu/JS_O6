a2e1c72add2c33c8424b41ad7e50ef4e679f19ce
Alina Zhu <alinazhu@Alinas-MacBook-Air.local>
2023-03-10 05:53:22 +0200

    changes

--- switch_array_loop03_07.js
+++ switch_array_loop03_07.js
@@ -25,3 +25,25 @@ if (login == true) {
 
 // ================= SWITCH ================
 
+let day = 3;
+switch(day){
+    case 2:
+        console.log("Tuesday");
+        console.log("OK")
+        break;
+    
+    case 3:
+        console.log("Wednesday");
+        console.log("OK")
+        break;
+    case 4:
+        console.log("Thursday");
+        console.log("OK")
+        break;
+    case 5:
+        console.log("Friday");
+        console.log("OK")
+        break;
+    default:
+        console.log("Another day")
+}
