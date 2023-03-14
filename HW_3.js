5f3fb1a160f76487338633e27b524eeaf360d191
aliluzu <alinazhu@Alinas-MacBook-Air.local>
2023-03-14 23:05:28 +0200

    files

--- HW_3.js
+++ HW_3.js
@@ -1,30 +1,30 @@
 // 1. Use SWITCH 
 // Write a simple calculator (*, /, +, -). Check your code for exception coverage for arithmetic operations.
-/*
-let num1 = 9;
-let num2 = 0
-let operator = "/";
 
-switch(operator){
-    case "+":
-        console.log(num1 + num2)
-        break;
-    case "-":
-        console.log(num1 - num2)
-        break;
-    case "*":
-        console.log(num1 * num2)
-        break;
-    case "/":
-    case num2 != 0:
-        console.log(num1 / num2)
-        console.log("cannot divide by zero")
-        break;
-    default:
-        console.log('Please enter a valid operator')
+// let num1 = 8.99;
+// let num2 = 2
+// let operator = "/";
 
-}
-*/
+// switch(operator){
+//     case "+":
+//         console.log(num1 + num2)
+//         break;
+//     case "-":
+//         console.log(num1 - num2)
+//         break;
+//     case "*":
+//         console.log(num1 * num2)
+//         break;
+//     case "/":
+//     case num2 == 0:
+//         console.log(num1 / num2)
+//         console.log("cannot divide by zero")
+//         break;
+//     default:
+//         console.log('Please enter a valid operator')
+
+// }
+
 
 // 2.Using loop "for" print event numbers from 2 to 10
 
@@ -83,22 +83,37 @@ if(counter == 0) {
 }
 */
 
+/*
+let array = [ '(', ')', '(', ')', ')'];
+let counter = 0;
+for(let i = 0; i < array.length; i ++) {
+  if(array[i] == '(') {
+    counter++
+  } else if(array[i] == ')') {
+    counter--
+  }
+}
+if(counter == 0) {
+  console.log("у каждой скобки есть пара");
+} else if (counter < 0 || counter > 0) {
+  console.log("скобочка осталась без пары...");
+}
+*/
+
 // 4. 
 //      *
 //     ***
 //    *****
 //   ******* 
 //  *********
-
 /*
+*/
 
-let str = "*********";
 
-for(let i = 1; i <= 7; i++){
-    let result = "";
-    for(let j = 1; j <= i; j++){
-        result += " "+ str[j-1]
+for(let i = 1; i <= rows; i++){ // loop for row
+    let str = " "
+    for(let k = 1; k <=(rows - 1); k++){
+      str += " "
     }
     console.log(result)
 }
-*/
