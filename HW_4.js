0ad100258922e5ea6280e9bcf610e5673f7e197a
aliluzu <alinazhu@Alinas-MacBook-Air.local>
2023-03-20 22:10:48 +0200

    files

--- HW_4.js
+++ HW_4.js
@@ -36,13 +36,3 @@ for(let i = 1; i <= rows; i++){
 }
 console.log(str)
 
-// for(let i  = 1; i <= rows; i++){
-//   for(let space = 1; space <= rows; space++){
-//     pattern += " ";
-//   }
-//   for(let j = 1; j <= i; j++){
-//     pattern += j
-//   }
-//   pattern += "\n"
-// }
-// console.log(pattern)
