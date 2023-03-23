5517dc5d91cbb49a6008c7a65606bb1765921c1b
TG-BX6RFG3\Alina Zhuravskaja <alina.zhuravskaja@tglab.com>
2023-03-23 16:45:35 +0200

    hw4

--- HW_4.js
+++ HW_4.js
@@ -88,7 +88,7 @@ console.log(str)
 // 4 8 11 13 
 // 5 9 12 14 15
 
-
+/*
 
 let str = ""
 let rows = 5                                             // 1 lopp                           2 loop                                    3 lopp
@@ -105,3 +105,93 @@ for(let i = 1; i <= rows; i++){                          //i = 1                
                                                                                                                                    // str+= (3 + psace) + (7 + space) + (10 + space)
 }
 console.log(str)
+
+*/
+
+// 4. Продумайте код для создания сэндвичей с 3-мя начинками
+//  и выставите цену в зависимости от вида начинки.
+//   Используйте while and do.. while loops” - это задание с предыдущего потока,
+//    его разбор есть в видео, можете не делать, по желанию)
+
+
+
+// 5.  Усовершенствуйте решение задачи про улитку с помощью  наиболее подходящего цикла. 
+
+// let dayDistance = 3
+// let nigthDown = -2
+// let distance = 0
+// let days = 0
+// let wall = 5
+
+// // 1
+// /*
+// if ((distance += dayDistance) <= wall){
+//     days++
+//     console.log(`Current distance at the end of the day for day ${days} is ${distance} meter(s)`)
+//     distance += nigthDown
+// }
+
+// // 2
+
+// if ((distance += dayDistance) <= wall) {
+//     days++
+//     console.log (`Current distance at the end of the day for day ${days} is ${distance} meter(s)`)
+//     distance += nigthDown
+// }
+
+// // 3
+
+// if ((distance += dayDistance) <= wall) {
+//     days++
+//     console.log (`Current distance at the end of the day for day ${days} is ${distance} meter(s)`)
+//     distance += nigthDown
+// }
+
+// //4
+
+// if ((distance += dayDistance) <= wall) {
+//     days++
+//     console.log (`Current distance at the end of the day for day ${days} is ${distance} meter(s)`)
+//     distance += nigthDown
+// }
+// */
+
+// //IMPROVMENT
+
+// while((distance += dayDistance <= wall)){
+//     days++
+//     console.log (`Current distance at the end of the day for day ${days} is ${distance} meter(s)`)
+//     if(distance == wall) console.log('Finish!')
+//     else console.log('Go forward!')
+//     distance += nigthDown
+// }
+
+// 6. Задача с интервью*
+// У вас есть массив с тремя видами скобок, предположим 
+// [ ‘{‘, ‘)’, ‘(‘, ‘)’, ‘)’, ‘}, ‘[‘, ‘]’, ‘)’] . Количество элементов и последовательность может быть разной.
+// Нужно выяснить, все ли скобки являются парными  в правильной последовательности (открывающая и затем закрывающая). Также, скобки могут быть вложенными, но тем не менее, если последовательность соблюдена, то они считаются парными.
+//  Пример:
+// [ ')', '{', '(', '(', ')', ')', '}, '[', ']', '(' ]  - у двух нет пары (нарушена последовательность)
+// [ '{', '(', '(', ')', ')', '}, '[', ']' ]  - у всех есть пара
+// [ '{', '(', '(', ')', ')', '}, '[', '[', ')' ] - у трех нет пары
+
+let counOpen = 0
+let countClose = 0
+let result = 0
+
+let myArr = [ ')', '{', '(', '(', ')', ')', '}', '[', ']', '(' ]
+for (let i = 0; i < myArr.length; i++){
+    if(myArr[i] == '('){
+        counOpen += 1
+    }else if (myArr[i] == ')') countClose += 1
+
+    result = counOpen - countClose
+    if(result < 0 ){
+        console.log('Wrong sequance')
+    }break;
+    console.log(result)
+}
+
+if(counOpen == countClose){
+    console.log('equal')
+}else console.log('not equal')
