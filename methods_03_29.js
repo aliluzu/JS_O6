4cba9444a0a86bb83f427ef321ce30cfcddd1c48
aliluzu <aliluzu97@gmail.com>
2023-04-03 21:43:49 +0300

    codewerse

---
+++ methods_03_29.js
@@ -0,0 +1,22 @@
+// let arr = [80, 117, 115, 104, 45, 85, 112, 115]
+let arr = ['t', 'e', 's', 't']
+
+/*
+function check(a,x){
+    return a.indexOf(x) >= 0
+
+};
+console.log(check(arr, 112))
+*/
+
+// assert.strictEqual(check([66, 101], 66), true);
+// assert.strictEqual(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+// assert.strictEqual(check(['t', 'e', 's', 't'], 'e'), true);
+
+// ------------------2
+
+function check(a,x){
+    return a.includes()
+};
+
+console.log(check(arr, 's'))  
