3138d4e527972a4f230bacde92ac0382f130edcf
aliluzu <aliluzu97@gmail.com>
2023-04-23 20:58:25 +0300

    method reduce

--- codewars_04_21.js
+++ codewars_04_21.js
@@ -2,7 +2,7 @@
 
 
 let array = [1, 3, 78, 45, 61, 100, 1, 2]
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let arr = [1, 2, 3, 4]
 
 // let result = 0
 
@@ -10,5 +10,5 @@ let sum = array.reduce((acc, currVal) => acc + currVal, 0) //0 : like default va
 console.log(sum)           //output: 291
 
 let multiply = arr.reduce((acc, currVal) => acc * currVal, 1) //0 : like default value
-console.log(multiply)    // otput: 362880
+console.log(multiply)    // otput: 24
 
