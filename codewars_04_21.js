e8cf700bc5a73e6e8c1049a4db1731ca5fe87c12
aliluzu <aliluzu97@gmail.com>
2023-04-23 21:17:30 +0300

    method reduce

--- codewars_04_21.js
+++ codewars_04_21.js
@@ -1,17 +1,32 @@
 // ================= reduce =============
 
 
-let array = [1, 3, 78, 45, 61, 100, 1, 2]
+let array = [1, -3, -78, -45, -61, -100, -1, 2]
 let arr = [1, 2, 3, 4]
 
 // let result = 0
-
+/*
 let sum = array.reduce((acc, currVal) => acc + currVal, 0) //0 : like default value
 console.log(sum)           //output: 291
-
+*/
 // let multiply = arr.reduce((acc, currVal) => acc * currVal, 1) //0 : like default value
 // console.log(multiply)    // otput: 24
 
-// ---------- avarage
-let multiply = arr.reduce((acc, currVal, index, array) => acc + currVal / array.length, 0) //0 : like default value
-console.log(multiply)
+// ---------- avarage ----------
+
+
+// 1st example
+// let sum = arr.reduce((acc, currVal, index, array) => acc + currVal / array.length, 0) //0 : like default value
+// console.log(sum)
+
+
+
+// 2nd example
+// let sum = arr.reduce((acc, currVal, index, array) => acc + currVal, 0) / array.length //0 : like default value
+// console.log(sum)
+
+
+//--------------Max value ---------------
+
+let maximum = array.reduce((acc, currVal, index, array) => Math.max(acc, currVal), -Infinity)
+console.log(maximum)
